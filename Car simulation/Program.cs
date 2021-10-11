23ce1f616aa4227e24fb8857cb021bc98c1819cf
Nijatasgar99 <nijataas@code.edu.az>
2021-10-11 17:07:18 +0500

    Добавьте файлы проекта.

---
+++ Car simulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Car_simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simulation Bmw = new Simulation("Bmw");
+            Bmw.burn();
+            Bmw.station();
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ Car simulation/simulation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_simulation
+{
+    class Simulation
+    {
+        string Bmw;
+        string color;
+        int tank;
+        private int loadingpetrol; // benzin yuklemek
+        private int burngasoline; // benzin yandirmaq
+        private int unused;//istifade edilmemis benzin
+        private int fueltank; // polnu bak 
+        private string powerhouse;   //at gucu     
+        private int speed;//suret
+
+
+
+        public Simulation()
+        {
+
+        }
+
+        public Simulation(string Cartype)
+        {
+            if (Cartype== "Bmw") ;
+            {
+                
+                fueltank = 40;
+                unused = 40;
+                burngasoline = 0;
+                Console.WriteLine("no need for gasoline");
+            }
+            
+        }
+        public void  station()
+        {
+            if (burngasoline>unused)
+            {
+                Console.WriteLine("refuel the car");
+            }
+            else
+            {
+
+                Console.WriteLine("Fuel");
+            }
+
+        }
+        public void burn()
+        {
+            if (speed>=100&&speed<=200)
+            {
+                burngasoline = 06;
+                Console.WriteLine("xerclediyi benzin miqdari"+burngasoline);
+            }
+            else
+            {
+                if (speed>=150&&speed<=300)
+                {
+                    burngasoline = 1;
+                    Console.WriteLine( "xerclediyi benzin miqdari"+burngasoline);
+                }
+            }
+        }
+
+
+    }
+   
+
+}
